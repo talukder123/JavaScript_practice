b23850504716c9d92327bebd2329caf063ce629e
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-16 21:58:03 +0600

    problem added

---
+++ largest.js
@@ -0,0 +1,13 @@
+/**
+ * check the largest number between two
+ */
+
+let a = 10,  b = 50;
+
+if (a > b) {
+    console.log(a, 'is greater than', b);
+} else if (b > a) {
+     console.log(a, 'is smaller than', b);
+} else {
+     console.log(a, 'is equal to', b);
+}
---
+++ positive_negative.js
@@ -0,0 +1,13 @@
+/**
+ * check a number is negative or positive or zero
+ */
+
+let num = 23478;
+
+if (num < 0) {
+    console.log(num, 'is negative');
+} else if (num > 0) {
+    console.log(num, 'is positive');
+} else {
+    console.log(num, 'is neither negative or positive');
+}
