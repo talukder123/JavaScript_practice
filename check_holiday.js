6b91a33ec83e04ac0564100873940dc30c05beb1
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-16 22:13:58 +0600

    problem added

---
+++ check_holiday.js
@@ -0,0 +1,9 @@
+/**
+ * Using isWeekend and isHoliday, print "No work today" if either one is true (use ||)
+ */
+let isHoliday = true;
+let isWeekend = false;
+
+if ((isHoliday) || (isWeekend)) {
+    console.log("No work today");
+}
---
+++ log_in.js
@@ -0,0 +1,15 @@
+/**
+ * Build a simple login system — print "Login successful" only if both username and password are correct.
+ */
+
+
+let username = true;
+let password = true;
+
+console.log('LOG-IN-PAGE');
+
+if ( (username) && (password) ) {
+    console.log("Login successful");
+} else {
+    console.log("Login failed");
+}
