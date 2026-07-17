5a51eefd9e33f4475c982781c3e139ee3fc7cbbd
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-17 22:10:08 +0600

    files moved

---
+++ loop/for_loop.js
@@ -0,0 +1,11 @@
+/**
+ * printing all the odd numbers between 1-100
+ */
+
+console.log('\n-- For Loop --\n');
+
+for (let i = 0; i <= 100; i++) {
+    if ( i%2 !== 0) {
+        console.log(i, '\n');
+    }
+}
