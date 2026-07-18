06b2309b0359341636d3ec1baf8fe0f2d79bfa22
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-18 22:19:21 +0600

    added

---
+++ array/array.js
@@ -0,0 +1,11 @@
+const player = ['MESSI', 'YAMAL', 'MBAPPE'];
+
+let n = player.length;
+
+// without loop
+console.log(player)
+
+//with loop
+for (let i =0; i<n; i++) {
+    console.log(player[i], '\n')
+}
---
+++ array/push.js
@@ -0,0 +1,11 @@
+const player = ['MESSI', 'YAMAL', 'MBAPPE'];
+
+player.push('Ronaldo');
+
+console.log(player); // push function
+
+let n = player.length;
+
+for (let i =0; i<n; i++) {
+    console.log(player[i], '\n')
+}
