de54dd5c6d620d5cae09026b72a79fe8feb0cda9
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-18 23:23:38 +0600

    slide added

---
+++ array/reverse.js
@@ -0,0 +1,5 @@
+const arr = [10, 20, 30, 40, 50];
+
+const ans = arr.reverse();
+
+console.log(ans);
---
+++ array/splice.js
@@ -0,0 +1,6 @@
+const arr = [10, 20, 30, 40, 50];
+
+let temp = arr.splice(2, 2, 99);
+
+console.log(temp);
+console.log(arr);
