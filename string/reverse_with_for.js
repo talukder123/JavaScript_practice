e1394101be4b4441f3ab79fa0c5a836b2f4c95ed
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-21 22:03:21 +0600

    add

---
+++ string/reverse_with_for.js
@@ -0,0 +1,9 @@
+const s = 'Messi is the GOAT';
+
+let ans = "";
+
+for (const i of s) {
+    ans = i + ans;
+}
+
+console.log(ans);
---
+++ string/split.js
@@ -0,0 +1,11 @@
+const companies = "Facebook Apple Amazon Netflix Google"
+
+
+console.log ('split by empty space -> extract all chs');
+console.log (companies.split(''));
+
+console.log ('split by space -> make into array');
+console.log (companies.split(' '));
+
+console.log ('split by ch');
+console.log (companies.split('a'));
