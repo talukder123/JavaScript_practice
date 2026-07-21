381da6dbfdcb9bcad490bbb171b9261499818a29
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-21 21:19:15 +0600

    add

---
+++ string/ends_with.js
@@ -0,0 +1,7 @@
+const username = 'talukder_x'
+
+if (username.endsWith('x')) {
+    console.log('valid');
+} else {
+    console.log('invalid');
+}
---
+++ string/starts_with.js
@@ -0,0 +1,7 @@
+const username = '@yurei-x'
+
+if (username.startsWith('@')) {
+    console.log('twitter[x] account');
+} else {
+    console.log('other account');
+}
